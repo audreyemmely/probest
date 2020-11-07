dd7a029486425d10768604e0b941e2b81f720dde
Audrey <aerv@ic.ufal.br>
2020-11-07 13:30:07 -0300

    função "barplot" adicionada na primeira questão

--- respostas-atividade1.R
+++ respostas-atividade1.R
@@ -1,22 +1,17 @@
 #Fazendo a leitura dos dados e selecionando o municÃ­pio a ser trabalhado nesse script
 dados <- subset(read.csv('https://raw.githubusercontent.com/audreyemmely/probest/main/dados_pandemia_alagoas.csv'), municipio_residencia == "Rio Largo")
 
-#Boxplot
-boxplot(dados$idade~dados$sexo, col = "gray", xlab = "Sexo", ylab = "Idade", main = "Boxplot - Idade e Sexo", pch = 19)
-boxplot(dados$idade, col = "gray", ylab = "Idade", main = "Boxplot - Idade", pch = 19)
-
-#1.
-#Gráfico de pizza
+#1. Escolha uma variável qualitativa e elabore um gráfico de barras e um gráfico de pizza.
 pie(table(dados$sexo), main = "Frequência dos dados de sexo", ylab="Frequência")
+barplot(table(dados$sexo), main = "Frequência dos dados de sexo", ylab="Frequência")
 
-#Gráfico de barra
-plot(table(dados$sexo), main = "Frequência dos dados de sexo", ylab="Frequência")
-
-#2.
-#Tabela de frequência
+#2. Escolha uma variável continua, no caso Idade, e elabore e a tabela de Frequências com os dados agrupados e o gráfico histograma.
 table(dados$idade) 
-
-#Histograma da tabela de frequencia
 hist(table(dados$idade), main = "Histograma da frequência dos dados da idade", xlab = "Dados", ylab="Frequência") 
 
+#3. Escolha duas variáveis qualitativa, sexo e situação atual, por exemplo e elabore alguns tipos de gráficos de barras.
 
+
+#4. Considere a variável qualitativa sexo e a variável quantitativa Idade e elabore os gráficos boxplots correspondentes.
+boxplot(dados$idade~dados$sexo, col = "gray", xlab = "Sexo", ylab = "Idade", main = "Boxplot - Idade e Sexo", pch = 19)
+boxplot(dados$idade, col = "gray", ylab = "Idade", main = "Boxplot - Idade", pch = 19)
