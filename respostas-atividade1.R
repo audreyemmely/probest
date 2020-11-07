8e659d3e66cc214bbc703af3737711e062444e67
Audrey <aerv@ic.ufal.br>
2020-11-07 13:57:17 -0300

    adicionando resolução da terceira questão

--- respostas-atividade1.R
+++ respostas-atividade1.R
@@ -1,4 +1,4 @@
-#Fazendo a leitura dos dados e selecionando o municÃ­pio a ser trabalhado nesse script
+#Fazendo a leitura dos dados e selecionando o municipio a ser trabalhado nesse script
 dados <- subset(read.csv('https://raw.githubusercontent.com/audreyemmely/probest/main/dados_pandemia_alagoas.csv'), municipio_residencia == "Rio Largo")
 
 #1. Escolha uma variável qualitativa e elabore um gráfico de barras e um gráfico de pizza.
@@ -10,7 +10,8 @@ table(dados$idade)
 hist(table(dados$idade), main = "Histograma da frequência dos dados da idade", xlab = "Dados", ylab="Frequência") 
 
 #3. Escolha duas variáveis qualitativa, sexo e situação atual, por exemplo e elabore alguns tipos de gráficos de barras.
-
+barplot(table(dados$situacao_atual))
+barplot(table(dados$sexo))
 
 #4. Considere a variável qualitativa sexo e a variável quantitativa Idade e elabore os gráficos boxplots correspondentes.
 boxplot(dados$idade~dados$sexo, col = "gray", xlab = "Sexo", ylab = "Idade", main = "Boxplot - Idade e Sexo", pch = 19)
