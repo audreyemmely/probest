6abeba74ce9007a7cff7dc878c5ba1c35af03f7b
Audrey <aerv@ic.ufal.br>
2020-11-11 18:14:24 -0300

    algumas alterações realizadas
    adicionando cor, alterando título dos gráficos

--- respostas-atividade1.R
+++ respostas-atividade1.R
@@ -2,8 +2,8 @@
 dados <- subset(read.csv('https://raw.githubusercontent.com/audreyemmely/probest/main/dados_pandemia_alagoas.csv'), municipio_residencia == "Rio Largo")
 
 #1. Escolha uma variável qualitativa e elabore um gráfico de barras e um gráfico de pizza.
-pie(table(dados$sexo), main = "Frequência dos dados de sexo", ylab="Frequência")
-barplot(table(dados$sexo), main = "Frequência dos dados de sexo", ylab="Frequência")
+pie(table(dados$sexo), main = "Gráfico de setores - Sexo", col = c("antiquewhite1", "lightcyan3"))
+barplot(table(dados$sexo), main = "Gráfico de barras - Sexo", ylab="Frequência",  col = c("antiquewhite1", "lightcyan3"))
 
 #2. Escolha uma variável continua, no caso Idade, e elabore e a tabela de Frequências com os dados agrupados e o gráfico histograma.
 Idade.tb<-(dados$idade)
@@ -20,12 +20,14 @@ freqRelAc <- cumsum(freqRel)
 TabResult = cbind(freq, freqAc, freqRel = round(freqRel*100, digits = 2), freqRelAc = round(freqRelAc*100, digits = 2))
 TabResult
 
-hist(Idade.tb, main = "Histograma da frequência da idade", xlab = "Dados", ylab="Frequência")  
+hist(Idade.tb, main = "Histograma - Idade", xlab = "Idade", ylab="Frequência")  
 
 #3. Escolha duas variáveis qualitativa, sexo e situação atual, por exemplo e elabore alguns tipos de gráficos de barras.
-barplot(table(dados$situacao_atual))
-barplot(table(dados$sexo))
+barplot(table(dados$sexo), main = "Gráfico de barras - Sexo", ylab="Frequência",  col = c("antiquewhite1", "lightcyan3"))
+
+barplot(table(dados$situacao_atual), main = "Gráfico de barras - Situação atual", col = gray.colors(8), names="")
+legend("topleft", legend=unique(dados$situacao_atual), cex=0.8, fill = gray.colors(8))
 
 #4. Considere a variável qualitativa sexo e a variável quantitativa Idade e elabore os gráficos boxplots correspondentes.
-boxplot(dados$idade~dados$sexo, col = "gray", xlab = "Sexo", ylab = "Idade", main = "Boxplot - Idade e Sexo", pch = 19)
-boxplot(dados$idade, col = "gray", ylab = "Idade", main = "Boxplot - Idade", pch = 19)
+boxplot(dados$idade~dados$sexo, col = c("antiquewhite1", "lightcyan3"), xlab = "Sexo", ylab = "Idade", main = "Boxplot - Idade e Sexo")
+boxplot(dados$idade, col = "moccasin", ylab = "Idade", main = "Boxplot - Idade")
